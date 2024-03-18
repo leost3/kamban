a9ba592fc39d01e1a1d36ab307f1035e08b7c93e
Leo <lstudart@pyx4.com>
2024-03-18 16:47:09 -0400

    add enpoints and respective tests

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,52 @@
-const express = require("express");
-const dotenv = require("dotenv");
-
+import dotenv from "dotenv";
+import express from "express";
+import pgp from "pg-promise";
+import { Board } from "./entity/Board";
+import { Card } from "./entity/Card";
+import { Column } from "./entity/Column";
 dotenv.config();
-
 const app = express();
 const port = process.env.PORT;
-//@ts-ignore
-app.get("/", (req, res) => {
-  res.send("Express + TypeScript Server");
-});
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
+app.get("/boards", async (req, res) => {
+  const connection = pgp()("postgres://postgres:postgres@localhost:5432/app");
+  const boardsData = await connection.query(
+    "select name, description from leo.board",
+    []
+  );
+  const boards: Board[] = [];
+  for (const boardData of boardsData) {
+    boards.push(new Board(boardData.name, boardData.description));
+  }
+  res.json(boards);
+});
+
+app.get("/boards/:boardId/columns", async (req, res) => {
+  const connection = pgp()("postgres://postgres:postgres@localhost:5432/app");
+  const columnsData = await connection.query(
+    "select name, has_estimation from leo.column where id_board = $1",
+    [req.params.boardId]
+  );
+  const columns: Column[] = [];
+  for (const columnData of columnsData) {
+    columns.push(new Column(columnData.name, columnData.has_estimation));
+  }
+  res.json(columns);
+});
+
+app.get("/columns/:columnId/cards", async (req, res) => {
+  const connection = pgp()("postgres://postgres:postgres@localhost:5432/app");
+  const cardsData = await connection.query(
+    "select title, estimation from leo.card where id_column = $1",
+    [req.params.columnId]
+  );
+  const cards: Card[] = [];
+  for (const cardData of cardsData) {
+    cards.push(new Card(cardData.title, cardData.estimation));
+  }
+  res.json(cards);
 });
---
+++ tests/integration/API.test.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+test("should return boards through an API", async () => {
+  const response = await axios({
+    method: "get",
+    url: "http://localhost:3000/boards",
+  });
+  const boards = response.data;
+  const [board] = boards;
+  expect(boards).toHaveLength(1);
+  expect(board.name).toBe("Project 1");
+  expect(board.description).toBe("TODO");
+});
+
+test("should return columns from a board through an API", async () => {
+  const response = await axios({
+    method: "get",
+    url: "http://localhost:3000/boards/1/columns",
+  });
+  const columns = response.data;
+  const [column1, column2, column3] = columns;
+  expect(columns).toHaveLength(3);
+  expect(column1.name).toBe("Column A");
+  expect(column2.name).toBe("Column B");
+  expect(column3.name).toBe("Column C");
+  expect(column1.hasEstimation).toBe(true);
+});
+
+test("should return card from a column through an API", async () => {
+  const response = await axios({
+    method: "get",
+    url: "http://localhost:3000/columns/1/cards",
+  });
+  const cards = response.data;
+  const [card1, card2, card3] = cards;
+  expect(cards).toHaveLength(3);
+  expect(card1.title).toBe("Activity 1");
+  expect(card2.title).toBe("Activity 2");
+  expect(card3.title).toBe("Activity 3");
+  expect(card1.estimation).toBe(10);
+  expect(card2.estimation).toBe(2);
+  expect(card3.estimation).toBe(4);
+});
